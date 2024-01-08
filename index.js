e6c73e6100768355596c33ea491f14cc4555892d
Marusya Bodrug <marusya.bodrug@gmail.com>
2024-01-08 17:40:52 +0100

    [] - Обчислюввальні властивості для невалідних ідентифікаторв

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-function greetingUser (user) {
-  user.age = 50;
-  return `Hey ${user.name} ${user.surname}. You will be ${user.age + 1} next year`
+ // [] = Обчислюввальні властивості для невалідних ідентифікаторв
+//  Невалідні ідентифікатори - коли починається з числа, або є пробіл
 
-}
-// Зміниться вік на 50
-const user = {
-  name: 'John',
-  surname: 'Doe',
-  age: 19
-}
-// Ми якщо приймаємо об'єкт в середині функції, то ми впливаємо на оригінальний об'єкт.
+ const user = {
+  name : 'John',  //ключ name : значення 'John'
+  'favourite color' : 'red',
+  0: 'value',
+ }
+ // Ключем в об'єкті може бути рядок або тип даних Symbol
+ // Ключ без лапок, JS автоматично оточує його лапками.
+
+ console.log(user.name); //John
+ console.log(user['favourite color']); // red
+ console.log(user[0]); // value 
+
+ // Валідні ідентифікатори доступ - .
+ // Невалідні ідентифікатори доступ - []
+
+ //////////////////////////////////////////////////////////////////////////
+
+ const someObject = {
+  2: 10
+ }
+
+ console.log(someObject[2]);
+
+ someObject[2] // VSC - допомагає
+
+ /*Задача 1 
+ 
+ Є об'єкт з різними версіями привітань для користувача
+ Вивести кор-чу привітання, в залежності від значення , яке він ввів*/
+
+ const greets = {
+  15: 'Hi',
+  17: 'Ya, what`s up!',
+  19: 'Hello',
+  30: 'Good morning!'
+ }
+
+ const valueFromUser = 19;
+ console.log(greets[19]); // AБО
+
+ console.log(greets[valueFromUser]);
