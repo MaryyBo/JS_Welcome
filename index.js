5edf7616bd5e9e0b4ebd77f95fe731f815ade803
Marusya Bodrug <marusya.bodrug@gmail.com>
2024-01-08 17:13:30 +0100

    Зміна об'єктів

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
+function greetingUser (user) {
+  user.age = 50;
+  return `Hey ${user.name} ${user.surname}. You will be ${user.age + 1} next year`
 
-//Задача. Всередині Alert виконати математичний розрахунок: 2 + 2 = 4
-
-// alert ('2 + 2 = ' + (2 + 2)); 
-
-// Подвійні лапки: "Привіт"
-// Одинарні лапки: 'Привіт'
-
-// Зворотні лапки: `Привіт` - мають розширену функціональність 
-// Вирази укладаються в $ {...вираз...}
-// Рядки , які знаходяться у взоротніх лапках `` називаються шаблонними
-
-// const nickName = 'Вася';
-
-// // Задача: створити Alert і привітатись з Васею
-
-// // alert ('Привіт,' + nickName + '!'); //Ми можемо скоротити цей вираз з допомогою зворотніх лапок
-
-// // alert (`Привіт , ${nickName}!`);
-
-// // alert (`результат: ${1+2}`);
-
-// // Запитати у користувача 2 числа і вивести alert
-
-// const first = Number(prompt ('Enter first number'));
-// const second = Number(prompt ('Enter second number'));
-
-// // alert(String (first) + '+' + String (second) + '=' + String (first + second)); // 5 + 10 = 15
-
-// alert (`${first} + ${second} = ${first + second}`);
+}
+// Зміниться вік на 50
+const user = {
+  name: 'John',
+  surname: 'Doe',
+  age: 19
+}
+// Ми якщо приймаємо об'єкт в середині функції, то ми впливаємо на оригінальний об'єкт.
