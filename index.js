6066894331fdef2a9898f9c944b989a50ec2d83b
Marusya Bodrug <marusya.bodrug@gmail.com>
2024-01-08 11:09:37 +0100

    Об'єкти

--- index.js
+++ index.js
@@ -1,57 +1,47 @@
-//Задача:
+//Об'єкти
 
-/*
-Написати функцію , яка обчислює факторіал переданого ій числа, результат
-роботи функції вивести на консолью.
+  /* 
 
-Факторіал - це 
-5! = 1*2*3*4*5
-10! 
-
-Якщо число відємне - поверніть з функції помилку
-Якщо число = 0 , або = 1 - поверніть з функції 1
-В іншому випадку - знаходимо факторіал. 
-
-
-*/
-
-function findFuctorial(number) {
-  let factorial = 1; // У цій змінній ми накопичуємо добуток
-
-  if (number < 0) {
-    throw Error('Number below 0 cant be factorial!');
-
-  } else if (number !== 0 && number !== 1) { 
-    for(i = 1; i <= number; i++) {
-      factorial = factorial * i;
-    }
+  const назваОб'єкту = {
+    ключ : значення
   }
 
-  return factorial;
-}
+  Об'єкти мають:
+   - властивості
+   - методи
+
+   const obj = {
+    color : 'white',
+    fontSize : 2
+   }
+
+  ООП - парадигма яка дозволяє створювати програми з використанням об'єктів
+  які мають властивості та методи 
+
+  Задача описати об'єкт ЛАМПОЧКА
+  */ 
+
+//   const bulb = {
+//     lightness: 200,
+//     power: '200W',
+//     cap: 'small',
+//     on: function (){ // анонімна функція юо в дужках пусто
+//         return 'The light on!';
+//     },
+//     off: function () {
+//         return 'The light off!';
+//     }
+//   }
+
+//   console.log(bulb.on()); // круглі дужки потрібні для виклику!
+//   console.log(bulb.lightness)
 
 
-function findFuctorial2(number) {
-  let factorial = 1; // У цій змінній ми накопичуємо добуток
+// Обє'кт dog
 
-  if (number < 0) {
-    throw Error('Number below 0 cant be factorial!');
-  } else if (number === 0 || number === 1) { 
-    return 1;
-  } else {
-    for(i = 1; i <= number; i++) {
-      factorial = factorial * i;
-    }
+  const dog = {
+    name: 'Tuzik',
+    'favorite food': 'meat',
   }
-
-  return factorial;
-}
-
-let number = 5;
-
-const result = findFuctorial(number);
-const result2 = findFuctorial2(number);
-
-
-console.log(result);
-console.log(result2);
+  console.log(dog.name);
+  console.log(dog ['favorite food']);
