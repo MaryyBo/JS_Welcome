2d2d1e479c202412588220b71580065ed0ea67a1
Marusya Bodrug <marusya.bodrug@gmail.com>
2024-01-08 15:01:04 +0100

    Задача, Об'єкт в об'єкті, Функція-конструктор

--- index.js
+++ index.js
@@ -1,32 +1,59 @@
 
-//Об'єкт КІТ
+/*Задача
+ВИзначте кота з властивостями і декількома методами:
+бігає
+спить
+мяукає
+
+*/
+
+// const cat = {
+//   name: 'Tom',
+//   color: 'Gigner',
+//   size: 'Middle',
+//   age: 1,
+//   run: function (){
+//     return 'I can run'
+//   },
+//   sleep: function () {
+//     return 'I can sleep'
+//   },
+//   meow: function () {
+//     return 'I can say \'meow!!!\''
+//   }
+// }
+
+// //Об'єкт в об'єкті -----------------------------------------------
+
+// cat.friend = {
+//   name: 'Luk',
+//   color: 'Black',
+//   size: 'Small',
+//   age: 4,
+// }
 
 
-const cat = {
-  name: 'Murzik',
-  color: 'Red',
-  breed: 'Dvorovyi',
-  age: 2,
-  eat: function () {
-    return 'I am eating'
-  },
-  sleep: function () {
-    return 'A am sleeping'
+// Конструктор визначає шаблон, скелет, об'єктів які створятсья 
+/* Існують угоди для створення конструктору
+1. Ім'я має починатися з ВЕЛИКОЇ літери.
+2. Функція цього конструктора має виконуватись лише за допомогою оператора new
+
+*/
+
+// Задача. Написати функцію конструктор для сворення кота
+
+function Cat (name, color, breed, age) {
+  this.name = name;
+  this.color = color;
+  this.breed = breed;
+  this.age = age;
+  this.run = function () {
+    return this.name + 'Is running'
   }
 }
 
-cat.age = cat.age + 1; // Змінили вік кота з 2 на 3
+// Задача. Створити декілько котів за допомогою конструктора
 
+const cat1 = new Cat ('Bublik', 'White', 'Dvorovyi' , 1 );
+const cat2 = new Cat ('Murzik', 'Black', 'Siam' , 4 );
 
-//Якщо потрібно видалити якийсь метод чи властивість - Оператор delete
-
-// delete cat.sleep;
-
-// Adding Properties
-// Задача. Додати ім'я друга Мурзика до об'єкту Мурзина
-
-cat.friend = 'Tuzik'
-cat['favourite food'] = 'Fish'; // Додавання невалідного ідентифікатору (через пробіл)
-cat.meow = function () {
-  return 'meow meow meow!'
-}
