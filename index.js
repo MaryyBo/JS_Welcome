76eb7b592c82f87a93a27bda63d8f97742a28297
Marusya Bodrug <marusya.bodrug@gmail.com>
2024-01-08 18:09:30 +0100

    Example - in

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
+function greeting (user) {
+  return `Hello ${'name' in user ? user.name : 'Anonym'}` ;
+}
 
- 
-
-//Чи є певний ключ?
-/*in - бінарний оператор (чи є в об'єкті певний ключ) повертає true/false
- 'key' in obj
-*/
-
-const obj = {
-  Mon: 'drive lesson',
-  Tue: 'dantist visit',
-  Wed: 'go to party',
-  Thu: 'hard work',
-  Fri: 'chill',
-  Sat: 'go to park'
- }
-
- console.log('Mon' in obj); // Mon - є в цьому об'єкті
+const user = {
+  email: 'john.doe@gmail.com',
+  name: 'Peter'
+}
