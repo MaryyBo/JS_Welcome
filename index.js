ee2e2140edabcb90895ab7c45e3f06a973a3c8b4
Marusya Bodrug <marusya.bodrug@gmail.com>
2024-01-08 18:21:32 +0100

    Цикл for...in  для об'єктів

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-function greeting (user) {
-  return `Hello ${'name' in user ? user.name : 'Anonym'}` ;
-}
+//Щоб перебрати значення які є в цьому об'єкті "Поштучно" є цикл FOR...IN
 
 const user = {
   email: 'john.doe@gmail.com',
-  name: 'Peter'
+  name: 'Peter',
+  age: 20,
+  lastname: 'Doe',
+  'favourite color': 'red',
+  movie : 'Blue moon',
+  avatar : 'http:...'
+
+}
+
+// for... in вик-я для перебору (ітерацій) ключів в об'єкті 
+
+/*
+for (variable in object) {
+  код, що виконується для кожної властивості
+}
+
+variable - змінна, яка буде приймати значення ключів властивостей
+object - об'єкт, властивості (ключі) якого ми хочемо перебрати
+
+
+*/
+//Задача. У об'єкті user вивести назви всіх ключів і властивості під цими ключами
+
+for (let key in user) { //key - це і є variable 
+  console.log(`${key} ---> ${user[key]}`); 
 }
