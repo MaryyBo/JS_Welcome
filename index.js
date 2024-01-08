9a21e3f2b0e54138b680ad0e3fc06fc86ec57c2a
Marusya Bodrug <marusya.bodrug@gmail.com>
2024-01-08 16:33:13 +0100

    Book Shop example задача

--- index.js
+++ index.js
@@ -1,59 +1,58 @@
+/*Cтворити конструктор об'єктів для представлення книг з наступними властивостями
+- Назва книги, автор, рік видання, ціна
+- Спроектувати методи для отримання і встановлення значень властивостей*/
 
-/*Задача
-ВИзначте кота з властивостями і декількома методами:
-бігає
-спить
-мяукає
+//ЗАДАЧА2. Зробити для всіх книг метод, який би повернув ціну книги, але зі знижкою
 
-*/
+function Book (title, author, year, price) {
+this.title = title;
+this.author = author;
+this.year = year;
+this.price = price;
 
-// const cat = {
-//   name: 'Tom',
-//   color: 'Gigner',
-//   size: 'Middle',
-//   age: 1,
-//   run: function (){
-//     return 'I can run'
-//   },
-//   sleep: function () {
-//     return 'I can sleep'
-//   },
-//   meow: function () {
-//     return 'I can say \'meow!!!\''
-//   }
-// }
+this.getTitle = function () {
+return this.title;
+} // Можем реалізувати якусь перевірку права доступу до властивості
+this.getAuthor = function () {
+return this.author;
+} // Можем реалізувати якусь перевірку права доступу до властивості
+this.getYear = function () {
+return this.year;
+} // Можем реалізувати якусь перевірку права доступу до властивості
+this.getPrice = function () {
+return this.price;
+} // Можем реалізувати якусь перевірку права доступу до властивості
 
-// //Об'єкт в об'єкті -----------------------------------------------
-
-// cat.friend = {
-//   name: 'Luk',
-//   color: 'Black',
-//   size: 'Small',
-//   age: 4,
-// }
-
-
-// Конструктор визначає шаблон, скелет, об'єктів які створятсья 
-/* Існують угоди для створення конструктору
-1. Ім'я має починатися з ВЕЛИКОЇ літери.
-2. Функція цього конструктора має виконуватись лише за допомогою оператора new
-
-*/
-
-// Задача. Написати функцію конструктор для сворення кота
-
-function Cat (name, color, breed, age) {
-  this.name = name;
-  this.color = color;
-  this.breed = breed;
-  this.age = age;
-  this.run = function () {
-    return this.name + 'Is running'
-  }
+this.setTitle = function (newTitle) {
+this.title = newTitle; //присвоїти нову назву (тут ще можуть бути перевірки по типу права доступу до властивості, або зміни назви)
+}
+this. setAuthor = function (newAuthor) {
+this.author = newAuthor;
+}
+this. setYear = function (newYear) {
+this.year = newYear;
+} // можна ще реалізувати , наприклад, якісь перевірки
+this. setPrice = function (newPrice) {
+this.price = newPrice;
+}
+this.culculateDiscauntedPrice = function (discountPercentage) {
+const discountedPrice = this.price - (this.price * (discountPercentage / 100));
+return discountedPrice;
 }
 
-// Задача. Створити декілько котів за допомогою конструктора
+}
 
-const cat1 = new Cat ('Bublik', 'White', 'Dvorovyi' , 1 );
-const cat2 = new Cat ('Murzik', 'Black', 'Siam' , 4 );
+const book1 = new Book ('Чотири вітри', 'Крістін Генна', 2020, 450)
+
+// console.log(book1.getTitle ());
+// console.log(book1.getAuthor());
+// console.log(book1.getPrice());
+
+// book1.setTitle('New Title'); //Поміняли значення!
+// book1.setAuthor('New Author');//Поміняли значення!
+// book1.setYear(2025);          //Поміняли значення!
+// book1.setPrice (340);         //Поміняли значення!
+
+// 
+// console.log(book1.culculateDiscauntedPrice(70));
 
