81e5824af8d4800aac38e777e7aeb12e29e5968b
MaryyBo <marusya.bodrug@gmail.com>
2024-01-12 19:53:21 +0100

    Arguments псевдо масив

--- index.js
+++ index.js
@@ -1,110 +1,37 @@
-
-// Задача 1
-// збільшити вік користувачів на 1
-
-//   const users = [{
-//     name: 'John',
-//     lastName: 'Doe',
-//     age: 19,
-//     email: 'john.doe@gmail.com'
-// }, {
-//     name: 'Jane',
-//     lastName: 'Doe',
-//     age: 22,
-//     email: 'jane.doe@gmail.com'
-// }, {
-//     name: 'Jackson',
-//     lastName: 'Doe',
-//     age: 55,
-//     email: 'jackson.doe@gmail.com'
-// }]
-
-// users.forEach((user) => { //Цей код коротший
-//   user.age += 1;
-// });
-
-// console.log(users);
-
-// АБО переписати так як нижче!
-
-// function plusOneYear (item) { //item - це елемент масиву
-//   item.age += 1;
-// }
-// users.forEach (plusOneYear);
-// console.log(users);
-
-// Задача 2
-
-// Дано масив:
-
-// const array = [2, 44, 11, 234, 8, 2, 4, 1];
-
-// Задача:
-// Зробити новий масив, всі елементи якого = елемент зі старого масиву + 100
-
-// Варіант 1
-// const newArray  = array.map((item) => {
-//     return item + 100;
-// })
-
-// Варіант 2
-//  function plus100 (item) { 
-//     return item + 100;
-//   }
-// console.log(array.map(plus100)); // можно користуватись цим
-
-
-// Задача 3
-/* Cтворити новий масив користувачів, об'єкти мають бути у такому вигляді
-{
-  fullname: 'John Doe',
-  email: ....
+function t() {
+  
+  arguments [4] = 'testvalue' // ми переоприділили
+  console.log(arguments)
 }
-*/
+t(1 , 2, 3, 4, 5);
 
-  const users = [{
-    name: 'John',
-    lastName: 'Doe',
-    age: 19,
-    email: 'john.doe@gmail.com'
-}, {
-    name: 'Jane',
-    lastName: 'Doe',
-    age: 22,
-    email: 'jane.doe@gmail.com'
-}, {
-    name: 'Jackson',
-    lastName: 'Doe',
-    age: 55,
-    email: 'jackson.doe@gmail.com'
-}]
+// console.log(t); // є консольлог, а є консольдір
 
-// 1 варіант
+// console.dir(t); // розгорнутий вид
+// arguments - не є масивом (псевдомасив), але його можно перетворити
 
-// const usersNew = users.map((user) => ({
-//   fullname: `${user.name} ${user.lastName}`,
-//   email: user.email
-// }));
-
-// console.log(usersNew);
-
-// 2 варіант
-// function newUserObject (user) {
-//   return {
-//     fullName: `${user.name} ${user.lastName}`,
-//     email: user.email
-//   }
+// function sum (a, b) {
+//   console.log(arguments);
+//   return a + b;
 // }
 
-// const newUsers1 = users.map(newUserObject);
+// console.log(sum (2, 6, 3, 4, 5, 6, 7));
 
-// 3 варіант
 
-// const newArrFromUsers = users.map((user) => {
-//   const newUser = { 
-//     fullName: `${user.name} ${user.lastName}`,
-//     email: user.email
-//   };
-//   return newUser;
-// });
+//Задача: Написати функціяю, яка повертає суму БУДЬ-ЯКОЇ кількости переданих в неї аргументів
 
+// Задача: написати функцію, яка повертає суму БУДЬ-ЯКОЇ кількості переданих в неї аргументів
+
+function sum() {
+  const arrayArgs = Array.from(arguments);
+
+  let sum = 0;
+
+  for(let i = 0; i < arrayArgs.length; i++) {
+    sum += arrayArgs[i];
+  }
+
+  return sum;
+}
+
+console.log(sum(2, 2, 2, 2, 2, 100));
