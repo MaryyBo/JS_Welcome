8cc9f3892bc4e7a2044b1f782b006820b783971e
MaryyBo <marusya.bodrug@gmail.com>
2024-01-15 18:14:51 +0100

    Array.reduce метод

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-/* Метод array.filter  - cтворює новий масив , який містить лише ті елементи які задовільняють певній умові (метод не мутуючий
-  array.filter (callback)
-  (callback)  - буде викликатись для кожного елементу масиву, і ця колбек функція в сеередині фільтр вона повинна повертати
-  або true/false для кожного елементу масиву
-  
-  */
- // задача: відфільтрувати масив щоб залишились тільки додатні числа
+/* аrray.reduce - використ для обчислення якогось ОДНОГО значення на основі всіх елементів масиву
+аrray.reduce - зводить всі елементи масиву до якогось одного результуючого значення (не мутуючий)
+аrray.reduce (сallback, initialValue)
+*/
 
-  const numbersArray = [5, 3 ,-9, 21, -5, 1, -2, 4];
+ const numbersArray = [2, 3, 1, 2, 34, 21];
 
-  function filterFunction (item) {
-  //   if (item > 0) {
-  //     return true;
-  //   } else {      // item < 0}
-  //     return false;
-  // } 
+ // Задача: знайти суму numbersArray
 
-  // можемо сокротити код !!!
-
-return item > 0
-}
-
-
-const filtredNumbersArray = numbersArray.filter (filterFunction);
+ function reducer (accumulator, item) { //  тaccumulator - типу змінної де накоплюється сума
+  return accumulator + item;
+ }
+ const sum = numbersArray.reduce(reducer, 0); // 0 - це початкове значення з якого ми починаємо рахувати суму
