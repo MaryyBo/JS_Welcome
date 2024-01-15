32673dc05b6bdf7d641fa871cbf4a9d97a8f7dec
MaryyBo <marusya.bodrug@gmail.com>
2024-01-15 19:05:20 +0100

    Задачі ???

--- index.js
+++ index.js
@@ -36,7 +36,49 @@ const dogArray = [{
 //   }
 // })
 
-dogArray.sort((dog1, dog2) => {
+// і навпаки 
 
-  return dog2.weight - dog1.weight ; // cортування у зворотньому порядку
-})
+// dogArray.sort((dog1, dog2) => {
+
+//   return dog2.weight - dog1.weight ; // cортування у зворотньому порядку
+// })
+
+
+// Відсортувати собак від найстаршого до наймолодшого
+
+// 1 варіант розв'язку
+
+// dogArray.sort ((dog1, dog2) => {
+//   if (dog1.age > dog2.age) {
+//     return -1;
+//   } else {
+//     return 1;
+//   }
+// })
+
+// // 2 варіант розв'язку
+// dogArray.sort ((dog1, dog2) => {
+// return dog1.age - dog2.age; 
+// })
+
+// Задача 2 Відсіяти всіх собак окрім білих
+
+
+// 1 варіант розв'язку
+
+dogArray.filter ((dog) => {
+  if (dog.color === 'white') {
+  return true;
+} else {
+  return false
+}
+});
+
+
+
+// або 
+
+// dogArray.filter ((dog) => {
+//   return dog.color === 'white'
+// });
+
