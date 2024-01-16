6c9b13231c8e89ac1ce68c1911e2cbc98e9b1287
MaryyBo <marusya.bodrug@gmail.com>
2024-01-16 18:02:02 +0100

    Літеральне створення прототипного посилання

--- index.js
+++ index.js
@@ -1,59 +1,38 @@
-function MyArray() {
-  this.length = 0;
 
-//   this.push = function(value) {
-//     // value - додати в КІНЕЦЬ масиву
-//     this [this.length] = value; 
-//     this.length++;
-//   } 
-//   return this.length
-// }
-
-// const arr = new MyArray();
-// arr.push (1);
-// arr.push (2);
-
-/*Перепишіть пуш таким чином, щоб пуш міг приймати та додавав 
-до масиву будь яку кількість переданих елементів */
-
-this.push = function() {
-  // arguments - додати в КІНЕЦЬ масиву
-  for (let i = 0; i < arguments.length; i++) {
-      this[this.length] = arguments[i];
-      this.length++;
-  }
-  return this.length;
+const cat = {
+  name: 'Barsik',
+  color: 'red',
+  age: 1
+  
 }
 
-this.pop = function() {
-  if (this.length > 0) {
-    //  1. зберегти останній елемент
-     const lastItem = this[this.length - 1]; 
-     // 2. видалити останній елемент з масиву
-     delete this [this.length - 1];
-     // 3. зменшити довжину масиву на 1
-     this.length--;
-     // 4. повернути останній елемент
-     return lastItem
-  } else {
-    return undefined;
+const cat2 = {
+  name: 'Murzik',
+  color: 'black',
+  age: 5
+  
+}
+
+const catMethods = { //Прототип
+  run: function () {
+    console.log('Cat is running');
+  },
+  meow: function () {
+    console.log('Meow');
+    
   }
 }
-this.forEach = function(callback) {
-  for (let i = 0; i < this.length; i++) {
-    callback(this[i], i, this);
-  }
-}
-}
 
-const arr = new MyArray();
-arr.push(1);
-arr.push(3, 2, 5, 10, 12, 33);
+//Як прикрутити  catMethods до об'єкту cat
 
+cat.__proto__ = catMethods; //прототипне посилання
+cat2.__proto__ = catMethods;
 
-//Відконсольлогувати квадрати кожного числа в масиві
-
-arr.forEach((item) => {
-  console.log(item * item) // або item **2 зведення до степені
-})
-
+/*console
+cat
+{name: 'Barsik', color: 'red', age: 1};
+cat2
+{name: 'Murzik', color: 'black', age: 5};
+cat.meow === cat2.meow
+true // одне посилання 
+*/
