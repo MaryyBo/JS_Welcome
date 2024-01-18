3a2687cb97b8457d61d6a5f069895459f5c79bbd
MaryyBo <marusya.bodrug@gmail.com>
2024-01-18 19:02:14 +0100

    Spread operator

--- index.js
+++ index.js
@@ -1,61 +1,43 @@
 'use strict';
 
-// function sum (a, b) {
-//     return a + b;
-// }
+/* 
+spread operator - розпаковувати елементи масиву, і використовувати це окремо
+[] --> .., .., .., .., ..,
+взяти кожен елемент з масиву і використовувати їх окремо
 
-// sum (3,4); // 7
-// sum (5,5); // 10
+*/
 
-// //АЛЕ !!! можна викликати функцію суми ось так
+const numbers = [1, 2, 3, 4, 5];
 
-// sum (1, 2, 3, 4); // 3 - тільки 2 аргументи враховуються!!!інші елементи попадають в arguments
-
- 
-/* Задача: У функції sum знайти суму двух елементів, а усі інші аругменти ,
- які передали у функцію ззовні, при виклику, їх потрібно покласти у масив
- 
- Тут нам допоможе REST оператор - rest ( залишок) він працює з залишками...
- ...,...,...,..., ->> [] rest покалде всі іншлі аргументи у масив
-
- ... трикрапка це REST
- */
-
-//  function sum (a, b, ...arrayOfRestArguments) { //Назву масиву новому даємо яку хочемо
-//     console.log(arrayOfRestArguments);
-//     return a + b;
-// }
-
-
-// console.log(sum (8, 2, 3, 4, 10, 4));  // (3, 4, 10, 4) - це все піде в новий масив!
-
-// Задача: Написати стрілочну функцію, яка сумує будь яку кількість чисел( rest оператор працює в стрілочниї функц)
-
-// const arrowSum = (...restArrayOfNumbers) => {
-//     let sum = 0;
-//     for (let i = 0; i< restArrayOfNumbers.length; i++) {
-//         sum += restArrayOfNumbers[i];
-//     }
-
-//     return sum; 
-// }
-
-
-// Задача тепер написати варіант функціі arrowSum з використанням методу reduce (передати стрілковий callback)
-
-
-
-const arrowSum = (...restArray) => { // 1 варіант виконання 
-
-    const sum = restArray.reduce ((accumulator, currentValue) => {
-        return accumulator + currentValue;
-    }, 0);
-    return sum;
+function sum (a, b, ...restArray) {
+    console.log(restArray);
+    return a + b;  // 1,2,3,4,5undefined (конкатенація)
 }
 
+// console.log(sum(numbers[0])); //Але це довго!
 
-// const arrowSum = (...restArray) =>  // 2 варіант виконання  (максимально короткий)
 
-//     restArray.reduce ((accumulator, currentValue) => accumulator + currentValue, 0);
+console.log(sum(...numbers)); //розпаковуємо всі значення всредеину йункції суми
+// ми розпакували всі значення в виклик функції sum
+
+// Як розрізняти ці ... 2 однакових оператори?
+// Якщо оператор стоїть в аргументах то це  - rest (збирає всі залишки параметрів)
+// Якщо ми використовуємо оператор на масиві то це - spread (розбиває масив на купу елементів)
+
+// Приклад використання spread operator #1
+
+const numbersMath = [2, 3, 1, 5, 4];
+
+Math.min(...numbersMath); //1
+
+// Приклад використання spread operator #2
+
+
+//Задача: зробити масив з назвою copyArray в який будуть входити всі елементи з масиву numbersMath
+
+const copyArray = [...numbersMath]; 
+
+// дозволяє скопіювати елементи з одного масиву в інший! (створює поверхневу копію, посилання на об'єкти одні і ті самі)
+
 
 
