a54e48d9b90dc7ba6ea56f4bf8b9b615be0d034a
MaryyBo <marusya.bodrug@gmail.com>
2024-01-23 18:41:44 +0100

    Errors

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
-// JSDoc - генератор документації 
+//Задача: Написати функцію суми додатніх чисел
 
 /**
- * Повертає х у ступені n
- * 
- * @param {number} x - це число , яке треба піднести до степеня
- * @param {number} n - це степінь, у яку потрібно піднести 
- * @return {number} x - піднесене у n-нну степінь
+ * Функція знаходить суму двох додатніх чисел
+ * @param {number} a перший доданок
+ * @param {number} b другий доданок
+ * @returns {number} сума
+ * @throws {RangeError} якщо a  або b - не є цілим числом
+ * @throws {TypeError}  якщо a  або b - не є числом
  */
 
-function pow (x, n) {
-    return x ** n;
 
+function sumOfPositiveNumber (a, b) {
+    if (a < 0 || b < 0) {
+     //маємо тут ! "викинути" помилку (RangeError - помилкиДіапазону)
+      const error = new RangeError('Якесь з чисел менше 0');
+      throw error; // Це типу return, тільки для помилок!
+    }
+
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new TypeError ('Параметри a та b мають бути числами')
+    }
+    return a + b;
 }
+
+
+try { // прописуєте якийсь код, очікуючи,що тут може виникнути помилка
+    sumOfPositiveNumber (1, -8);
+} catch (error) {
+    console.log(error);
+}
+
+console.log('Usual code flow');
+
+
+// Використовувати ПРОСТО Error, якщо не можемо обрати тип помилки. 
+
+// THROW
+// Синтаксис оператор throw <об'єкт_помилки>
+// Назва оператора перекладається як - кинути
+// Це типу return, тільки для помилок!
+
+// SyntaxError
+
+// & - Uncaught SyntaxError: Unexpected token '&' 
+// ...abracadabra - Uncaught SyntaxError: Unexpected token '...'
+
+
+// ReferenceError - не доступна в поточній області видимості
+
+// TypeError - проблеми з типом даних
+
+// sumOfPositiveNumber (1, -2);
