229f5c09190c3aa2f54b6057f90f73d2d68a7147
MaryyBo <marusya.bodrug@gmail.com>
2024-01-26 17:50:17 +0100

    Еxtends - розширення

--- index.js
+++ index.js
@@ -1,64 +1,30 @@
-// ВАЛІДАЦІЯ 
 
-const MIN_ZP = 7100;
-const WORK_DAYS = 21;
-const MIN_RATE = MIN_ZP / WORK_DAYS // Параметри за замовчуванням
-
-class Worker {
-    constructor (name, lastName, rate = MIN_RATE , days = WORK_DAYS) {
-
-        if (name === '' || lastName === '') {
-            throw new Error ('You must enter your name and last name')
-        }
-        this.name = name;
-        this.lastName = lastName;
-
-        if (typeof rate !== 'number' || typeof days !== 'number') {
-            throw new TypeError ('Rate and Days must be a number')
-        }
-
-        if ( rate < 0) {
-            throw new RangeError ('Rate must be a positive number')
-        }
-
-        this._rate = Number(rate.toFixed(2)); //округлення .toFixed(2)
-
-        if (days < 0 || days > 31) {
-            throw new RangeError ('Days must be in range 0 - 31')
-        }
-        this.days = days;
+class Animal { // батьківський класс
+    constructor (nickname, color) {
+        this.nickname = nickname;
+        this.color = color;
     }
 
-    setRate(value) {
-        if (typeof value !== 'number') {
-            throw new TypeError ('Rate must be a number')
-        }
-
-        if (value  < 0) {
-            throw new RangeError ('Rate must be a positive number')
-        }
-        this._rate = value;
-}
-
-    getRate () {
-        return this._rate;
+    eat () {
+        return `${this.nickname} is eatng`
     }
-
-    getSalary () {
-     
-            return this.rate * this.days;
-        }
-        
 }
 
 
-const worker1 = new Worker("Alex", "Dane", 150, 20);
-const worker2 = new Worker('Dari', 'Dane', 130, 23);
-const worker3 = new Worker('Savanna', 'Loe'); // за замовчуванням підставиться 
+// class Dog extends Animal клас Собаки розширює клас Тварини (наслідує цей клас)
 
-/* Поля, назва яких починається зі знаку нижнього підкреслення _name, то їх не можна змінювати вручну!
-Їх можна змінювати тільки за допомогою якогось методу set/get якщо потрібно втсановити якесь нове значення для поля 
-що починається з знаку нижнього підкреслення _name , треба користуватися setter (value) {})
+class Dog extends Animal { // extend - для створення наслідування між класами
+    constructor (nickname, color) { //дочірній клас
+       super (nickname,color) // для виклику конструктора його батьківського класу - Animal
+    }
 
-А є метод який буде повертати це нове значення getter () {}
-*/ 
+    eat () {
+        return `${this.nickname} is eatng`; //метод eat вже не треба, так , як він є у Animal
+    }
+
+    gav () {
+        return `${this.nickname} gav gav gav `;
+    }
+}
+
+const dog = new Dog ('Tuzik', 'grey');
