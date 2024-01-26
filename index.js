12af2e8e78a11b63f67d1534553d98a54015ecd2
MaryyBo <marusya.bodrug@gmail.com>
2024-01-26 16:14:55 +0100

    Валідація, Private _field

--- index.js
+++ index.js
@@ -1,39 +1,64 @@
-/* 
-У нас є 2 окремих класи: 
+// ВАЛІДАЦІЯ 
 
-Клас авто
-Клас паливо
+const MIN_ZP = 7100;
+const WORK_DAYS = 21;
+const MIN_RATE = MIN_ZP / WORK_DAYS // Параметри за замовчуванням
 
-Задача: порахувати загальну вагу автомобіля ( вага авто + вагапалива)
-Щоб порахувати вагу палива потрібно об'єм * щільність
-*/
+class Worker {
+    constructor (name, lastName, rate = MIN_RATE , days = WORK_DAYS) {
 
-class Fuel {
-    constructor (volume, density) {
-        this.volume = volume;
-        this.density = density;
-    }
-
-    getWeight () {
-        return this.volume * this.density;
-    }
-}
-
-const benzin = new Fuel (50, 0.9);
-
-class Auto {
-    constructor (name, ownWeight, fuel) {
+        if (name === '' || lastName === '') {
+            throw new Error ('You must enter your name and last name')
+        }
         this.name = name;
-        this.ownWeight = ownWeight;
-        this.fuel = fuel;
+        this.lastName = lastName;
+
+        if (typeof rate !== 'number' || typeof days !== 'number') {
+            throw new TypeError ('Rate and Days must be a number')
+        }
+
+        if ( rate < 0) {
+            throw new RangeError ('Rate must be a positive number')
+        }
+
+        this._rate = Number(rate.toFixed(2)); //округлення .toFixed(2)
+
+        if (days < 0 || days > 31) {
+            throw new RangeError ('Days must be in range 0 - 31')
+        }
+        this.days = days;
     }
 
-    // мето , який обчислює повну вагу авто: його власна вага + вага палива (вага палива в класі Fuel)
- getFullWeght () {
-    return this.ownWeight + this.fuel.getWeight(); // ЗАПИСАТИ сюди метод розрахунку з попереднього класу
- }
+    setRate(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError ('Rate must be a number')
+        }
 
-
+        if (value  < 0) {
+            throw new RangeError ('Rate must be a positive number')
+        }
+        this._rate = value;
 }
 
-const bmw = new Auto ('BMW', 4000, benzin); 
+    getRate () {
+        return this._rate;
+    }
+
+    getSalary () {
+     
+            return this.rate * this.days;
+        }
+        
+}
+
+
+const worker1 = new Worker("Alex", "Dane", 150, 20);
+const worker2 = new Worker('Dari', 'Dane', 130, 23);
+const worker3 = new Worker('Savanna', 'Loe'); // за замовчуванням підставиться 
+
+/* Поля, назва яких починається зі знаку нижнього підкреслення _name, то їх не можна змінювати вручну!
+Їх можна змінювати тільки за допомогою якогось методу set/get якщо потрібно втсановити якесь нове значення для поля 
+що починається з знаку нижнього підкреслення _name , треба користуватися setter (value) {})
+
+А є метод який буде повертати це нове значення getter () {}
+*/ 
