52f83226710386feba6fd0c2074ba2c8d2c4b740
MaryyBo <marusya.bodrug@gmail.com>
2024-01-29 19:46:55 +0100

    instanceof (Переваги/Недоліки ООП)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Успадкування - здатність до копіювання
 
-class Figure {
+class Figure { // Абстрактний клас 
     constructor (sideQuantity) {
         this.sideQuantity = sideQuantity;
     }
@@ -17,7 +17,7 @@ return this._sideQuantity;
         this._sideQuantity = newValue;
     }
 
-    getArea () {
+    getArea () { //абстрактний метод
 
     }
 }
@@ -47,7 +47,7 @@ class Triangle extends Figure {
         return this._b;
     }
      
-    set a (newValue) {
+    set b (newValue) {
         if (newValue < 0) {
             throw new RangeError('Side cannot be less than o')        
         }
@@ -65,14 +65,10 @@ class Triangle extends Figure {
     this._angle = newValue;
 }
 
-    getArea() {
+    getArea() { // віртулальний метод
         return this.a * this.b * Math.sin(this.angle);
     }
-
-
-
 }
-
 
 
 class Square extends Figure {
@@ -93,9 +89,41 @@ class Square extends Figure {
     }
 
 
-    getArea () {
+    getArea () { // віртулальний метод
         return this.a * this.a; // this.a **2
     }
 }
 
 const square = new Square(4);
+const triangle = new Triangle (5, 45, 6);
+
+
+
+// Написати окрему функцію , яка буде приймати об'єкт якоїсь фігури і буде викликати у ціє фігури метод getArea
+
+function getFigureArea (figure) {
+   if(figure instanceof Figure) {
+    return figure.getArea();
+   }
+   throw new TypeError('Параметр не є фігурою');
+}
+
+//instanceof - чи є якийсь об'єкт екземпляром певного класу. повертає true / false
+
+/*
+
+Переваги 
+
+1. Візуально код стає простішим
+2. Менше однакового коду (ми можемо перевикористовувати код, наслідуючи один від іншого)
+3. Складні програми пишуться простіше
+4. При наслідування ООП збільшується щвидкість написання (швидко можемо прийти до мінімально працюючого прототипу)
+
+Недоліки
+
+1. Важко зрозуміти і почати працювати (дуже багато треба знати)
+2. ООП потребує більше пам'яті
+3. Продуктивність коду може бути досить низька (із сучасними потужностями процессорів, недолік нівелюється)
+
+
+*/
