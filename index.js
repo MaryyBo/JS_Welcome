d93d46a321122226ae37092453262fb78af58ec5
MaryyBo <marusya.bodrug@gmail.com>
2024-02-02 19:27:15 +0100

    Map

--- index.js
+++ index.js
@@ -1,71 +1,66 @@
+/*
+Map (карта, мапа, словник)
+Зберігає пари : "ключ : значення" (як і об'єкт)
+має розмір - size
 
-const arr = [2, 4, 1, 3, 7, 3, 2, 1, 3, 5, 2, 8, 2, 4, 1]
+Відмінності від звичайного об'єкт
 
-//Реалізувати функцію лінійного пошуку якогось зачення в масиві.
+1. Запам'ятовує елементи в тому ж порядку в якому ми їх додаємо, порядок важливий, 
+2. Ключем може бути будь-якийтип даних (А неттак як у об'єкті може бути тип даних String чи Symbol)
+(Об'єкти, в тому числі функції, передаються за посиланнями)
 
-function linearSearch (array, value) { //лінійна складність алгоритму
-    for (let i = 0; i < array.lenght; i++) {
-        if (array[i] === value) {
-            return i;
+Ключ в колекції Map має бути унікальним
+*/
+
+const map = new Map(); // зберігаємо пари ключ значення, але в них ще є розмір - size
+
+// set - задає значення
+map.set(1, {});
+map.set('1', 'value');
+
+//get - для отримання значення по якомусь ключу
+
+map.get('1');
+
+//has - чи Є якийсь певний ключ в Map
+
+map.has('1') //true
+
+const vocabulary = new Map();
+
+vocabulary.set ('cat', 'кіт');
+vocabulary.set ('dog', 'собака');
+vocabulary.set ('eat', 'їсти');
+
+/*
+Написати функцію яка приймає строку англійською мовою і преекладає українською,
+використовуючи словник.
+*/
+
+function translator (str, vocabulary) {
+
+    console.log(str);
+    
+    const arrayWords = str.toLowerCase().trim().split(' ') // split зробить масив
+    
+    console.log(arrayWords);
+    //у нас є словник translator. Треба перебрати масив слів arrayWords і переклад для кожнгого англ слова дістати з нашого словника vocabulary
+    const translatedArray = arrayWords.map((word) => {
+        if(vocabulary.has(word)) {
+          return vocabulary.get(word);
+        } else {
+          return word;
         }
+      });
+    
+      return translatedArray.join(' ');
     }
-    return -1; // -1 повертається значення коли ми не знайшли елемент , який нам потрібен
-
-}
-
-// console.log(linearSearch(arr, 2));
+    
+    console.log(translator('    Cat eat dog    ', vocabulary));
 
 
-// Лінійна складність коли Big O, тобто коли у нас стільки ітерацій (операцій), скільки у нас вхідних даних
-
-
-// Приклад квадратичної складності
-// Вивести таблицю множення 
-
-function multyTable (limit){
-    const table = [];
-    for (let i = 1; i <= limit; i++) {
-        for (let j = 1; j <= limit; j++) {
-            table.push (`${i} * ${j} = ${i * j} `)
-        }
-    }
-    return table;
-}
-
-// Квадратична складність, коли ми на віході отримуємо кількість вхідних даних тобто n в квадраті
-
-// multyTable(1); // 1 (1^2 = 1)
-// multyTable(2); // 4 (2^2 = 4)
-// multyTable(3); // 9 (3^2 = 9)
-
-// НЕ бажано використовувати квадратичну складність тобто цикл у циклі, подвійний цикл.
-
-// Логарифмічна складність 
-// на 100 вхідних даних припадає приблизно 6 операцій!
-
-const arr2 = [2, 3, 4, 5, 6, 7, 8, 9, 10]; // має бути порядок по збільшенню, відсортований масив!!! (можна ділити масив навпіл, скорочуєчи кількість операцій)
-
-function binarySearch (array, whatToFind) { //чи може бути число в масиві взагалй
-if (whatToFind > array[array.lenght - 1]) {
-return - Infinity; // такого числа точно немає в масиві
-}
-let start = 0;
-let end = array.lenght - 1;
-let middle = Math.round((start + end) / 2); 
-
-while (true) {
-    if(array [middle] === whatToFind) {
-        return middle;
-    } if (array [middle] < whatToFind) {
-        start = middle;
-        middle = Meth.ceil((start + end) / 2); // якшо середній елемент менший за шуканий
-   
-    } else {
-        end = middle;
-        middle = Math.ceil((start + end) / 2); 
-    }
-}
-}
-// Є косяки!!!!Не працює 
-
-
+// методи строк str
+// str.trim() - прибере пробіли
+// str.toLowerCase() - все буде маленькими буквами
+// split(' ') // split зробить масив
+// join - з масиву зробить строку
