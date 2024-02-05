8fe75eab8fdbe7123abae04149b002d84da78730
MaryyBo <marusya.bodrug@gmail.com>
2024-02-05 19:19:58 +0100

    Practice task resolve Set

--- index.js
+++ index.js
@@ -1,41 +1,13 @@
-/*set - це ніби сосуд, колекція УНІКАЛЬНИХ значень, елементів, кожен елем в цьому сосуді можезустрічатися тільки один раз. 
-Він схожий на масив чи на мапу, але сет не допускає наявності дублікатів.Якщо додаєм значення 
-до set воно буде збережено лише одинраз 
-
-set - множина (множество) набір уникальних значень
-
-Значення додаються за допомогою методу -  .add
 
 
-*/
+ //Задача: 
 
-const set = new Set();
+ const arrWithDoubles = [2, 3, 3, 5, 6, 77, 77, 5, 1, 1, 12, 12, 13]
+ // повернути масив без повторень
 
-// set.prototype.add - додає новий елемент із заданим значенням у кінеуь об'єкта Set
+//  const uniqueArr = new Set(arrWithDoubles);
 
-set.add(1);
-set.add(4);
-set.add('Hello');
-set.add('Hello'); // Дублікати НЕ додаються в Set!!!
+ const newArr = [...new Set(arrWithDoubles)];
 
-// set.prototype.has - перевіряє наявність елементу у множині (setі)
+console.log(newArr);
 
-console.log(set.has(1)); //true
-console.log(set.has(12)); //false
-
-// set.prototype.delete - удаляє елемент з множини 
-
-set.delete('Hello');
-
-const set2 = new Set([2,3,4,5]);
-
-// з set можно зробити масив
-
-// set.prototype.values - використов для отримання ітератора який повертає нам всі значення з об'єкта set
- const valuesIterator = set.values();
-
- valuesIterator.next().value; // 1
- valuesIterator.next().value; // 4
- valuesIterator.next().value; // undefined, done: true
-
- const arrayFromSet = [...set.values()];
