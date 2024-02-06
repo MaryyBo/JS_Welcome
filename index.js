dd55e099430330d260a7069713ac859d9469b7fe
MaryyBo <marusya.bodrug@gmail.com>
2024-02-06 18:50:08 +0100

    3 вида ДЕСТРУКТУРИЗАЦІЇ у JS

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
-// У нас є об'єкт юзера
-const user = {
-    name: 'John',
-    age: 30,
-    address: {
-        city: 'Kyiv',
-        country: 'Ukraine'
-    },
-    contacts: {
-        email: 'john@gmail.com',
-        phone: '+380994875717'
-    }
+/* Існує 3 вида ДЕСТРУКТУРИЗАЦІЇ у JS
+
+1.Деструктуризація об'єктів
+2.Деструктуризація вхідних параметрів
+3.Деструктуризація масивів (зустрічається рідко)
+
+*/
+
+const arr = [1, 2, 3, 4, 5, 6];
+
+// треба отримати - arr[0];
+
+// const firstElement = arr[0]; // можемо зробити так
+// console.log(firstElement);
+
+const [firstElement, secondElement, thirdElement, ...restOfArr] = arr;
+
+
+//2.Деструктуризація вхідних параметрів
+
+function getFullName ({firstName, lastName, ...restObject}) {  //({}) ---> тут будемо приймати об'єкт
+    console.log(restObject); // все, що залишилось йде в restObject!!
+return `${firstName} ${lastName}`; // все інше окрім {firstName, lastName} - ігнорується
 }
 
-//отримати значення name/city/email/ phone
+const user2 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 42,
+    geolocation: '42.34343546 56.7565342',
+    browser: 'Chrome'
+}
 
-const {name, address: {city}, contacts: {email, phone}} = user;
-console.log(name);
-console.log(city);
-console.log(email);
-console.log(phone);
+console.log(getFullName(user2)); // передаємо цілий масив і у відповідь через функцію все інше окрім {firstName, lastName} - ігнорується
+
