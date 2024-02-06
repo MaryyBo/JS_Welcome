38fc8dc5227142bd7c6d25afc23390496b1ee3ac
MaryyBo <marusya.bodrug@gmail.com>
2024-02-06 18:02:50 +0100

    Деструктуризація - задачка

--- index.js
+++ index.js
@@ -1,52 +1,21 @@
-const monitor = {
-    sizes: {
-        height: {
-            value: 30,
-            scale: 'cm // 1'
-        },
-        width: {
-            value: 50,
-            scale: 'cm // 2'
-        }
+// У нас є об'єкт юзера
+const user = {
+    name: 'John',
+    age: 30,
+    address: {
+        city: 'Kyiv',
+        country: 'Ukraine'
     },
-    brightness: 750,
-    refresh: {
-        value: 144,
-        scale: 'Grc'
-    },
-    color: 'black',
-    resolution: '4K'
+    contacts: {
+        email: 'john@gmail.com',
+        phone: '+380994875717'
+    }
 }
 
-// console.log(monitor.color);
-// console.log(monitor.sizes.height.value); // прокладаємо шлях до value, але це НЕ ЗРУЧНО
+//отримати значення name/city/email/ phone
 
-// // const height = monitor.sizes.height.value; // можно зберегти значення в змінну, і використовувати по призначенню
-// console.log(height);
-
-// //Але для цього є ДЕСТРУКТКРИЗАЦІЯ (спеціальний синтаксис присвоєння, витягує властивості з об'єкту)
-
-// const {resolution, color} = monitor; // якщо треба декілька властивостей, то можна перерах ії через кому
-// console.log(resolution);
-// console.log(color);
-
-// const {resolution, color: monitorColor} = monitor; // якщо хочему змінити назву color і щоб в змінну записалось нове ім'я color: monitorColor
-
-
-// Задача: Витягнути value для висоти та ширини монітору
-
-// const {sizes: {height: {value: heightValue}, width: {value: widthValue} }, brightness, refresh: {value: refreshValue}} = monitor; // переназвали змінну на heightValue
-// console.log(heightValue); // heightValue - це тепер змінна
-
-// // const {sizes: {width: {value: widthValue}}} = monitor; 
-// console.log(widthValue); //widthValue - це тепер змінна
-// console.log(refreshValue);
-// console.log(brightness);
-
-// const {sizes: {height: {scale: scaleHeight}, width: {scale: scaleWidth} }} = monitor; 
-// console.log(scaleHeight);
-// console.log(scaleWidth);
-
-
-const {color, brightness, resolution, ...restOfMonitor} = monitor;
-//...restOfMonitor - все інше записалось у рест об'єкт
+const {name, address: {city}, contacts: {email, phone}} = user;
+console.log(name);
+console.log(city);
+console.log(email);
+console.log(phone);
